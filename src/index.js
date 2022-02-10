6c073c30e20166021843536ff07ab003658510be
Vasyl Zvarych <zvaricvasil@gmail.com>
2022-02-10 23:44:48 +0200

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const decode ={
+        '00': '',
+        '10': '.',
+        '11': '-'}
+    var res = '';
+    for (let a = 0; expr.length > 0;) {
+        let str = expr.substring(0, 10);
+        if (str[0] == '*') {
+            res = res + ' ';
+        } else {
+            let code = '';
+            for (let i = 0; i < 10; i=i+2) {
+                if (str.substring(i, i + 2) == '10') {
+                    code = code + '.';
+                } else if (str.substring(i, i + 2) == '11') {
+                    code = code + '-';
+                }
+            }
+            res = res + MORSE_TABLE[code];
+        }
+        expr = expr.slice(10);
+    }
+    return res;
 }
 
+
 module.exports = {
-    decode
+decode
 }
